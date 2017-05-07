ab91163da68383e49e101926dce80cbbba3387d9
cw585968 <christian_wassermann@web.de>
2017-05-07 20:13:07 +0200

    Presses Ctrl + shift + f, java doc update, most important fix see comments
    added if statement in recursive remove function
    if (n.parent.left == n) {
        n.parent.left = null;
    } else {
        n.parent.right = null;
    }

--- src/Hausaufgaben05/TreeNode.java
+++ src/Hausaufgaben05/TreeNode.java
@@ -1,15 +1,17 @@
 /**
+ * Class TreeNode resembles one node in a tree
+ * 
  * Created by Tom on 06.05.2017.
  */
 public class TreeNode {
-    public int value;
-    public TreeNode right, left, parent;
+	public int value;
+	public TreeNode right, left, parent;
 
-    public TreeNode(int value, TreeNode parent){
-        this.value = value;
-        this.parent = parent;
-        this.right = null;
-        this.left = null;
-    }
+	public TreeNode(int value, TreeNode parent) {
+		this.value = value;
+		this.parent = parent;
+		this.right = null;
+		this.left = null;
+	}
 
 }
