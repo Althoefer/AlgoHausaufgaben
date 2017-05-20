3702360d071e0cc3c7bad769dce14b2d01750f6a
cw585968 <christian_wassermann@web.de>
2017-05-20 11:58:01 +0200

    Changed swap to swap to indices instead of values, added java doc

--- src/Hausaufgaben07/Heap.java
+++ src/Hausaufgaben07/Heap.java
@@ -2,33 +2,78 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Class Heap implements a field embedded heap
+ * 
+ * @author C.Wassermann
+ *
+ */
 public class Heap {
 
+	// field embedded heap
+	// from child to parent: childIndex / 2
+	// from parent to left child: parentIndex * 2
+	// from parent to right child: parentIndex * 2 + 1
 	private List<Integer> heap;
 
+	/**
+	 * Default constructor which creates an empty heap
+	 */
 	public Heap() {
+		// new field with dummy element
 		this.heap = new ArrayList<>();
 		this.heap.add(null);
 	}
 
+	/**
+	 * Check if the heap is empty
+	 * 
+	 * @return whether the heap is empty or not
+	 */
 	public boolean isEmpty() {
+		// only dummy element in heap
 		return this.heap.size() == 1;
 	}
 
+	/**
+	 * Add a new element to the heap
+	 * 
+	 * @param i
+	 *            value which is added to the heap
+	 */
 	public void add(int i) {
+		// simply adding to the field so the heap
+		// remains left complete (heap condition)
 		this.heap.add(i);
+		// reestablish heap condition (sift up)
 		upheap();
 	}
 
-	public int getMax() {
+	/**
+	 * Remove the largest value from the heap
+	 * 
+	 * @return the largest value
+	 * @throws IndexOutOfBoundsException
+	 *             if heap is already empty
+	 */
+	public int getMax() throws IndexOutOfBoundsException {
+		if (isEmpty()) {
+			throw new IndexOutOfBoundsException("Heap is already empty!");
+		}
+		// remove first element and replace with last
+		// so the heap remains left complete (heap condition)
 		int result = this.heap.get(1);
 		int lastIndex = this.heap.size() - 1;
-		swap(this.heap.get(1), this.heap.get(lastIndex));
+		swap(1, lastIndex);
 		this.heap.remove(lastIndex);
+		// reestablish heap condition (sift down)
 		downheap();
 		return result;
 	}
 
+	/**
+	 * ToString method: Get a textual representation of the heap
+	 */
 	public String toString() {
 		Iterator<Integer> iter = this.heap.iterator();
 		iter.next(); // skip dummy value
@@ -47,19 +92,23 @@ public class Heap {
 		}
 	}
 
+	// sift up to reestablish heap condition
 	private void upheap() {
 		if (isEmpty()) {
 			return;
 		}
+		// initialize variables with valid values
 		int currentIndex = this.heap.size() - 1;
 		int current = this.heap.get(currentIndex);
-		int parentIndex = (currentIndex) / 2; // int division intended
+		int parentIndex = (currentIndex) / 2; // integer division intended
 		if (parentIndex < 1) {
 			return;
 		}
 		int parent = this.heap.get(parentIndex);
+		// swap parent of current & current as long as
+		// parent is smaller than current (heap condition)
 		while (parent < current) {
-			swap(parent, current);
+			swap(parentIndex, currentIndex);
 			currentIndex = parentIndex;
 			parentIndex = (currentIndex) / 2;
 			if (parentIndex < 1) {
@@ -69,11 +118,13 @@ public class Heap {
 		}
 	}
 
+	// sift down to reestablish heap condition
 	private void downheap() {
-		// middle
 		if (isEmpty()) {
 			return;
 		}
+		// initialize variables with valid values
+		// middle
 		int currentIndex = 1;
 		int current = this.heap.get(currentIndex);
 		// left
@@ -88,12 +139,14 @@ public class Heap {
 		if (this.heap.size() > rightIndex) {
 			right = this.heap.get(rightIndex);
 		}
+		// swap with larger child value as long as
+		// parent is not yet larger than both child value
 		while (current < left || current < right) {
-			if (left > right) {
-				swap(current, left);
+			if (left > right) { // left bigger
+				swap(currentIndex, leftIndex);
 				currentIndex = leftIndex;
-			} else {
-				swap(current, right);
+			} else { // right bigger
+				swap(currentIndex, rightIndex);
 				currentIndex = rightIndex;
 			}
 			// left
@@ -111,10 +164,10 @@ public class Heap {
 		}
 	}
 
+	// swap element at index i and j
 	private void swap(int i, int j) {
-		int posj = this.heap.indexOf(j);
-		int posi = this.heap.indexOf(i);
-		this.heap.set(posj, i);
-		this.heap.set(posi, j);
+		int temp = this.heap.get(i);
+		this.heap.set(i, this.heap.get(j));
+		this.heap.set(j, temp);
 	}
 }
--- src/Hausaufgaben07/TestHeap.java
+++ src/Hausaufgaben07/TestHeap.java
@@ -1,7 +1,13 @@
-
+/**
+ * Test Class for Heap
+ * 
+ * @author C.Wassermann
+ *
+ */
 public class TestHeap {
 
 	public static void main(String[] args) {
+		// could also take multiple same values
 		int[] itemsToAdd = new int[] { 1, 6, 8, 18, 23, 5, 17, 20, 26, 21, 9 };
 		Heap heap = new Heap();
 		System.out.println(heap);
@@ -13,6 +19,8 @@ public class TestHeap {
 			System.out.println(heap.getMax());
 			System.out.println(heap);
 		}
+		// System.out.println(heap.getMax());
+		// --> Exception is thrown correctly
 	}
 
 }
