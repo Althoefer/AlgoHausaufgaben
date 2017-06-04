11bbd06cd5262f836d1f601e4e3c69612ba8805b
cw585968 <christian_wassermann@web.de>
2017-06-04 21:55:13 +0200

    Changed Zeile and Spalte so it is correct now
    Tests still complete successfully

--- src/Hausaufgaben09/Damenproblem.java
+++ src/Hausaufgaben09/Damenproblem.java
@@ -41,52 +41,51 @@ public class Damenproblem {
 	private boolean damenProblem(int spalte, int[][] brett) {
 		// all queens are already placed, this is a solution!
 		if (spalte >= brett.length) {
-			// printBrett(brett);
 			return true;
 		}
 		// go through all lines in the column and try to place a queen
-		for (int i = 0; i < brett.length; i++) {
-			if (validPos(spalte, i, brett)) { // position is valid, place the queen
-				brett[spalte][i] = 1;
+		for (int zeile = 0; zeile < brett.length; zeile++) {
+			if (validPos(zeile, spalte, brett)) { // position is valid, place the queen
+				brett[zeile][spalte] = 1;
 				// if there is a solution for the next queens too, pass that on
 				if (damenProblem(spalte + 1, brett)) {
 					printBrett(brett);
 				}
 				// clear the placed queen again
-				brett[spalte][i] = 0;
+				brett[zeile][spalte] = 0;
 			}
 		}
 		return false;
 	}
 
 	/**
-	 * checks if the given position on a given board is a valid position for a
-	 * queen
+	 * checks if the given position on a given board is a
+	 * valid position for a queen
 	 * 
-	 * @param spalte
-	 *            the column to be check
 	 * @param zeile
-	 *            the line to be checked
+	 *            the line to be check
+	 * @param spalte
+	 *            the column to be checked
 	 * @param brett
 	 *            the board, where we check the position
 	 * @return true if we can place a queen on the given position, false if not
 	 */
-	public boolean validPos(int spalte, int zeile, int[][] brett) {
+	public boolean validPos(int zeile, int spalte, int[][] brett) {
 		// check if there is already a queen in the line
 		for (int j = 0; j < spalte; j++) {
-			if (brett[j][zeile] != 0) {
+			if (brett[zeile][j] != 0) {
 				return false;
 			}
 		}
 		// check if there is a queen in the upper left diagonal
 		for (int i = zeile, j = spalte; i >= 0 && j >= 0; i--, j--) {
-			if (brett[j][i] != 0) {
+			if (brett[i][j] != 0) {
 				return false;
 			}
 		}
 		// check for a queen in the lower left diagonal
 		for (int i = zeile, j = spalte; i < brett.length && j >= 0; i++, j--) {
-			if (brett[j][i] != 0) {
+			if (brett[i][j] != 0) {
 				return false;
 			}
 		}
@@ -102,7 +101,7 @@ public class Damenproblem {
 	public void printBrett(int[][] brett) {
 		for (int i = 0; i < brett.length; i++) {
 			for (int j = 0; j < brett.length; j++) {
-				System.out.print(brett[j][i] + " ");
+				System.out.print(brett[i][j] + " ");
 			}
 			System.out.println();
 		}
